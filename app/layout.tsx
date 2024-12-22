47d36827335b7a999237c48c9db2c5716201337d
Mano <112639423+Manavalan2517@users.noreply.github.com>
2024-12-22 16:28:05 +0530

    figma clone

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,42 +1,30 @@
-import type { Metadata } from "next";
-import {Work_Sans} from "next/font/google";
-import "./globals.css";
-import { Room } from "./Room";
+import { Work_Sans } from "next/font/google";
 
-//const geistSans = localFont({
-//  src: "./fonts/GeistVF.woff",
-//  variable: "--font-geist-sans",
-//  weight: "100 900",
-//});
-//const geistMono = localFont({
-//  src: "./fonts/GeistMonoVF.woff",
-//  variable: "--font-geist-mono",
-//  weight: "100 900",
-//});
+import "./globals.css";
+import { TooltipProvider } from "@/components/ui/tooltip";
+
+import Room from "./Room";
+
+export const metadata = {
+  title: "Figma Clone",
+  description:
+    "A minimalist Figma clone using fabric.js and Liveblocks for realtime collaboration",
+};
 
 const workSans = Work_Sans({
   subsets: ["latin"],
   variable: "--font-work-sans",
-  weight: ['400', '600', '700']
-})
+  weight: ["400", "600", "700"],
+});
 
-export const metadata: Metadata = {
-  title: "figmaClone",
-  description: "",
-};
+const RootLayout = ({ children }: { children: React.ReactNode }) => (
+  <html lang='en'>
+    <body className={`${workSans.className} bg-primary-grey-200`}>
+      <Room>
+        <TooltipProvider>{children}</TooltipProvider>
+      </Room>
+    </body>
+  </html>
+);
 
-export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
-  return (
-    <html lang="en">
-      <body className={`${workSans.className} bg-primary-grey-200`}>
-        <Room>
-          {children}
-        </Room>
-      </body>
-    </html>
-  );
-}
+export default RootLayout;
--- components/cursor/CursorChat.tsx
+++ components/cursor/CursorChat.tsx
@@ -1,14 +1,7 @@
-import CursorSVG from "@/public/assets/CursorSVG";
 import { CursorChatProps, CursorMode } from "@/types/type";
-import React from "react";
+import CursorSVG from "@/public/assets/CursorSVG";
 
-const CursorChat = ({
-  cursor,
-  cursorState,
-  setCursorState,
-  updateMyPresence,
-}: CursorChatProps) => {
-
+const CursorChat = ({ cursor, cursorState, setCursorState, updateMyPresence }: CursorChatProps) => {
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     updateMyPresence({ message: e.target.value });
     setCursorState({
@@ -40,10 +33,8 @@ const CursorChat = ({
         transform: `translateX(${cursor.x}px) translateY(${cursor.y}px)`,
       }}
     >
-      {/* Show message input when cursor is in chat mode */}
       {cursorState.mode === CursorMode.Chat && (
         <>
-          {/* Custom Cursor shape */}
           <CursorSVG color="#000" />
 
           <div
@@ -53,15 +44,7 @@ const CursorChat = ({
               borderRadius: 20,
             }}
           >
-            {/**
-             * if there is a previous message, show it above the input
-             *
-             * We're doing this because when user press enter, we want to
-             * show the previous message at top and the input at bottom
-             */}
-            {cursorState.previousMessage && (
-              <div>{cursorState.previousMessage}</div>
-            )}
+            {cursorState.previousMessage && <div>{cursorState.previousMessage}</div>}
             <input
               className="z-10 w-60 border-none	bg-transparent text-white placeholder-blue-300 outline-none"
               autoFocus={true}
